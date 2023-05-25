4ce111aa7eaf9938fe07cb1aa57562712b13ae40
mertasta <stanislav.merta@quorumsoftware.com>
2023-05-25 13:33:55 +0200

    fix: remove unused import

--- android/src/main/java/com/hemangkumar/capacitorgooglemaps/CustomMapView.java
+++ android/src/main/java/com/hemangkumar/capacitorgooglemaps/CustomMapView.java
@@ -9,7 +9,6 @@ import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.util.Consumer;
 
